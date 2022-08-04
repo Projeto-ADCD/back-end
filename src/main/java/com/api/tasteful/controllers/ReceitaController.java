0ea4d556d4ac2db38cc9cf07600ee95ac2b442a6
rafaela-amorim <rafaela.silva@ccc.ufcg.edu.br>
2022-08-04 13:40:37 -0300

    Comecei a documentar as rotas no receita controller

--- src/main/java/com/api/tasteful/controllers/ReceitaController.java
+++ src/main/java/com/api/tasteful/controllers/ReceitaController.java
@@ -13,35 +13,57 @@ import org.springframework.web.bind.annotation.RestController;
 import com.api.tasteful.entities.Receita;
 import com.api.tasteful.services.ReceitaService;
 
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 public class ReceitaController {
-	
+
 	@Autowired
 	private ReceitaService receitaService;
-	
+
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Por enquanto só diz hello world!")
+	})
 	@GetMapping("/")
 	public ResponseEntity<String> helloworld() {
 		return new ResponseEntity<String>("hello world", HttpStatus.OK);
 	}
 	
+
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Retorna uma lista com todas as receitas do banco de dados")
+	})
 	@GetMapping("/receitas")
 	public ResponseEntity<List<Receita>> getReceitas() {
 		return new ResponseEntity<List<Receita>>(receitaService.getReceitas(), HttpStatus.OK);
 	}
 	
+	@ApiParam(
+		    name =  "id",
+		    type = "Integer",
+    		value= "ID único de uma receita no banco de dados",
+		    required = true)
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Retorna a receita correspondente ao ID único")
+	})
 	@GetMapping("/receitas/{id}")
 	public ResponseEntity<Receita> getReceitaById(@PathVariable Integer id) {
 		return new ResponseEntity<Receita>(receitaService.getReceitaById(id), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/pesquisa")
-	public ResponseEntity<List<Receita>> getReceitaByNome(@RequestParam(value = "nomeReceita", required = false) String nomeReceita) {
+	public ResponseEntity<List<Receita>> getReceitaByNome(
+			@RequestParam(value = "nomeReceita", required = false) String nomeReceita) {
 		return new ResponseEntity<List<Receita>>(receitaService.getReceitaByNome(nomeReceita), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/pesquisa/filtro")
-	public ResponseEntity<List<Receita>> filtrarReceitas(@RequestParam(value = "ingredientes", required = false) String[] ingredientes, 
-													@RequestParam(value = "nao_ingredientes", required = false) String[] nao_ingredientes) {
-		return new ResponseEntity<List<Receita>>(receitaService.filtrarReceitas(ingredientes, nao_ingredientes), HttpStatus.OK);
+	public ResponseEntity<List<Receita>> filtrarReceitas(
+			@RequestParam(value = "ingredientes", required = false) String[] ingredientes,
+			@RequestParam(value = "nao_ingredientes", required = false) String[] nao_ingredientes) {
+		return new ResponseEntity<List<Receita>>(receitaService.filtrarReceitas(ingredientes, nao_ingredientes),
+				HttpStatus.OK);
 	}
 }
