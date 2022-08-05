3e064e9da7ae27ba86777482c35f8c27ba8e3fed
matheus lisboa <matheus.lisboa.santos@ccc.ufcg.edu.br>
2022-08-04 21:42:38 -0300

    rotas do ReceitaController todas foram descritas pra documentação swagger

--- src/main/java/com/api/tasteful/controllers/ReceitaController.java
+++ src/main/java/com/api/tasteful/controllers/ReceitaController.java
@@ -30,7 +30,6 @@ public class ReceitaController {
 	public ResponseEntity<String> helloworld() {
 		return new ResponseEntity<String>("hello world", HttpStatus.OK);
 	}
-	
 
 	@ApiResponses(value = { 
 			@ApiResponse(responseCode = "200", description = "Retorna uma lista com todas as receitas do banco de dados")
@@ -47,22 +46,43 @@ public class ReceitaController {
 	public ResponseEntity<Receita> getReceitaById(
 			@ApiParam(
 				    name =  "id",
-				    type = "Integer",
+//				    type = "Integer",
 		    		value= "ID único de uma receita no banco de dados",
 				    required = true)
 			@PathVariable Integer id) {
 		return new ResponseEntity<Receita>(receitaService.getReceitaById(id), HttpStatus.OK);
 	}
 
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Retorna uma lista de Receitas que contenham a palavra chave (ou algo similar) no título.")
+	})
 	@GetMapping("/pesquisa")
 	public ResponseEntity<List<Receita>> getReceitaByNome(
+			@ApiParam(
+				    name =  "nomeReceita",
+				    type = "String",
+		    		value= "palavras chave que o usuário espera encontrar como título da receita",
+				    required = false)
 			@RequestParam(value = "nomeReceita", required = false) String nomeReceita) {
 		return new ResponseEntity<List<Receita>>(receitaService.getReceitaByNome(nomeReceita), HttpStatus.OK);
 	}
 
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Retorna uma lista de receitas que **obrigatoriamente** contém a lista de *ingredientes* e **não contém** a lista de *nao_ingredientes*.")
+	})
 	@GetMapping("/pesquisa/filtro")
 	public ResponseEntity<List<Receita>> filtrarReceitas(
+			@ApiParam(
+				    name =  "ingredientes",
+				    type = "String",
+		    		value= "palavras chave que o usuário espera encontrar na lista de ingredientes da receita (embora esteja sendo buscado tanto no título quanto no corpo da receita também).",
+				    required = false)
 			@RequestParam(value = "ingredientes", required = false) String[] ingredientes,
+			@ApiParam(
+				    name =  "nao_ingredientes",
+				    type = "String",
+		    		value= "palavras chave que o usuário quer que *não* estejam presentes na receita.",
+				    required = false)
 			@RequestParam(value = "nao_ingredientes", required = false) String[] nao_ingredientes) {
 		return new ResponseEntity<List<Receita>>(receitaService.filtrarReceitas(ingredientes, nao_ingredientes),
 				HttpStatus.OK);
